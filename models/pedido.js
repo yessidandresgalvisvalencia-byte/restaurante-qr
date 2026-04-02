3f8654c5b3e8151951dd8ffde5d39ef2c432b50f
yessid <tucorreo@gmail.com>
2026-04-02 12:52:01 -0500

    subiendo proyecto completo

---
+++ models/pedido.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose")
+
+const pedidoSchema = new mongoose.Schema({
+    restaurantId: {
+        type: String,
+        default: "rest1"
+    },
+    sedeId: {
+  type: String,
+  default: ""
+},
+    mesa: {
+        type: Number,
+        required: true
+    },
+    observaciones: {
+  type: String,
+  default: ""
+},
+    producto: {
+        type: String,
+        required: true
+    },
+    categoria: {
+        type: String,
+        default: ""
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    estado: {
+        type: String,
+        enum: ["pendiente", "preparando", "listo", "entregado"],
+        default: "pendiente"
+    },
+    tiempoEstimado: {
+        type: Number,
+        default: 15
+    },
+    metodoPago: {
+        type: String,
+        enum: ["efectivo", "transferencia", "pse", "tarjeta"],
+        default: "efectivo"
+    },
+    estadoPago: {
+        type: String,
+        enum: ["pendiente", "pagado"],
+        default: "pendiente"
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Pedido", pedidoSchema)
